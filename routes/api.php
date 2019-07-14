b0042777513ae26d5fcd650089b08fee2eda2627
alicfeng <a@samego.com>
2019-07-14 21:29:14 +0800

    update:review

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dingo\Api\Routing\Router;
 use Illuminate\Http\Request;
 
 /*
@@ -13,20 +14,22 @@ use Illuminate\Http\Request;
 |
 */
 
+/**
+ * @var Dingo\Api\Routing\Router $api
+ */
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'prefix'    => config('admin.route.prefix'),
-    'namespace' => 'App\Http\Controllers\Api',
+    'prefix'     => config('admin.route.prefix'),
+    'namespace'  => 'App\Http\Controllers\Api',
     'middleware' => 'serializer:array',//返回数据结构为 ArraySerializer
-], function ($api) {
-    $api->post('/user/login','UserController@login');//用户登录
+], function (Router $api) {
+    $api->post('/user/login', 'UserController@login');//用户登录
     // 需要 token 验证的接口
-    $api->group(['middleware' => config('admin.route.middleware')], function ($api) {
+    $api->group(['middleware' => config('admin.route.middleware')], function (Router $api) {
         $api->post('/user/logout', 'UserController@logout');//退出登录
         $api->get('/user/info', 'UserController@info');//用户详情信息
         $api->get('test', 'TestController@test');//测试
-        $api->get('/template-manager/list','TemplateManagerController@list');//模版列表
+        $api->get('/template-manager/list', 'TemplateManagerController@list');//模版列表
     });
-
 });
